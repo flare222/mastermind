d64d2b961f895b216c98ed799fdeac4beb14ee01
Clare Robertson <clare.robertson@gaincredit.com>
2020-04-22 10:59:36 +0100

    create game board, generate random solution, identify colour from palette

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,45 @@
 function init() {
 
-  const grid = document.querySelector('.grid')
-  const squares = []
+  const instructions = document.querySelector('.instructions')
+  const tbl = document.querySelector('.table')
+  const tblBody = document.createElement('tbody')
   const solution = document.querySelector('.solution')
   const sols = []
-  // const sol = document.querySelector('sol-item')
-  const randomSol = (Math.floor(Math.random() * sols.length))
-  
-  const width = 4
-  const height = 10
+  const paletteItems = document.querySelectorAll('input[name=radio]')
+
   const colours = ['purple-bead', 'teal-bead', 'seagreen-bead', 'blue-bead', 'coral-bead', 'yellow-bead']
   let randomColour = (Math.floor(Math.random() * colours.length))
 
-  // ! Game Grid
-  Array(width * height).join('.').split('.').forEach(() => {
-    const square = document.createElement('div')
-    square.classList.add('grid-item')
-    squares.push(square)
-    grid.appendChild(square)
-  })
+  // ! Create Game Board
+  function generateTable() {
+    // creating all cells
+    for (let i = 0; i < 10; i++) {
+      // creates a table row
+      const row = document.createElement('tr')
+
+      for (let j = 0; j < 4; j++) {
+        // creates a cell
+        const cell = document.createElement('td')
+        cell.classList.add('bead')
+        row.appendChild(cell)
+      }
+      // add the row to the end of the table body
+      tblBody.appendChild(row)
+      tblBody.firstChild.classList.add('row-selected')
+    }
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody)
+  }
+
+  generateTable()
 
   // ! Solution Grid
-  Array(width * 1).join('.').split('.').forEach(() => {
+  Array(4 * 1).join('.').split('.').forEach(() => {
     const sol = document.createElement('div')
     sol.classList.add('sol-item')
     sols.push(sol)
     solution.appendChild(sol)
   })
-
-  console.log(colours[randomColour])
-  console.log(randomSol)
 
   function generateSolution() {
     for (let i = 0; i < sols.length; i++) {
@@ -40,5 +50,62 @@ function init() {
 
   generateSolution()
 
+  // ! Choose Colour from palette
+  function chooseColour() {
+    for (let i = 0; i < paletteItems.length; i++)  {
+      if (paletteItems[i].checked) {
+        paletteItems[i].checked = false
+        const colour = paletteItems[i].value
+        console.log(colour)
+        console.log(paletteItems[i].checked)
+      }
+    }
+  }
+  
+  paletteItems.forEach(() => {
+    addEventListener('click', chooseColour)
+  })
+  
+  console.log(paletteItems)
+
+  // ! Change Colours in guess row
+
+  
+
+  // ! Make a guess
+  function guess() {
+    //click on a tick button to confirm the colour combination
+    //move the selected class to the next row
+  }
+
+  // ! Toggle Instructions
+  function toggle() {
+    document.querySelector('.instructions').classList.toggle('show')
+  }
+
+  //! Event Listeners 
+
+  instructions.onclick = function() {
+    toggle()
+  }
+
 }
 window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+// ! Create Game Board
+// const grid = document.querySelector('.grid')
+// const rows = []
+// const beads = []
+// const width = 1
+// const height = 10
+
+// Array(width * height).join('.').split('.').forEach(() => {
+//   const row = document.createElement('div')
+//   row.classList.add('row')
+//   rows.push(row)
+//   grid.appendChild(row)
+    
+// })
